ddaf467c43e00adb5371aac4a60379cdd1693024
Ganei-ux <alyodlyodl@gmail.com>
2025-02-05 16:03:21 +0300

    تحديث التطبيق

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>مرحبًا بك في موقعي الأول!</h1>
+        <h1>مرحبًا بك في اليلي الأول!</h1>
         <p>تم نشر هذا الموقع باستخدام GitHub Pages</p>
       </header>
     </div>
